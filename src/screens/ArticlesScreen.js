b4c72d570996c27147b0d3d47656c40afa68c8f7
PawelRadawiec <pawel.radaw@gmail.com>
2021-10-26 19:21:17 +0200

    app navigator, article screen

---
+++ src/screens/ArticlesScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, View, Text } from 'react-native';
+
+const ArticlesScreen = () => {
+  return (
+    <View>
+      <Text>Articles screen</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ArticlesScreen;
