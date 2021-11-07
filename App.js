5ef89870a1ea4f841537a9fc56315d006e44373a
PawelRadawiec <pawel.radaw@gmail.com>
2021-11-07 14:32:59 +0100

    navigation - switch navigator with bottom tab navigator

--- App.js
+++ App.js
@@ -1,24 +1,41 @@
 import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
+import { createSwitchNavigator } from 'react-navigation';
+import { createBottomTabNavigator } from 'react-navigation-tabs';
 import { ArticleProvider } from './src/context/ArticleContext';
 import ArticleDetailsScreen from './src/screens/ArticleDetailsScreen';
 import ArticlesScreen from './src/screens/ArticlesScreen';
+import ArticleCreate from './src/screens/ArticleCreateScreen';
+import AccountScreen from './src/screens/AccountScreen';
 
-const navigator = createStackNavigator(
-  {
-    Articles: ArticlesScreen,
-    Details: ArticleDetailsScreen,
-  },
-  {
-    initialRouteName: 'Articles',
-    defaultNavigationOptions: {
-      title: 'Top articles',
+const switchNavigator = createSwitchNavigator({
+  mainScreens: createBottomTabNavigator({
+    Articles: {
+      screen: createStackNavigator({
+        Articles: ArticlesScreen,
+        Details: ArticleDetailsScreen,
+      }),
+      navigationOptions: {
+        title: 'Top articles',
+      },
     },
-  }
-);
+    Create: {
+      screen: ArticleCreate,
+      navigationOptions: {
+        title: 'Add article',
+      },
+    },
+    Account: {
+      screen: AccountScreen,
+      navigationOptions: {
+        title: 'Account',
+      },
+    },
+  }),
+});
 
-const App = createAppContainer(navigator);
+const App = createAppContainer(switchNavigator);
 
 export default () => {
   return (
---
+++ src/screens/ArticleCreateScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const ArticleScreen = () => {
+  return (
+    <View>
+      <Text>Article createŽ</Text>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({});
+
+export default ArticleScreen;
